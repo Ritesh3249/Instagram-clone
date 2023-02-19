2c99e17e52d3fe6df5ab280dc5112e61c8021daf
Ritesh kumar <riteshgupta3249@gmail.com>
2023-02-19 12:38:45 +0530

    created login and register desiging

--- src/page/login/Login.js
+++ src/page/login/Login.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import logo from "../../assests/download.png"
 import "./login.css"
 const Login = () => {
   return (
-    <div >
-        <div>
-            <div classNma></div>
+    <div className='loginBody  displayFlex justifyContentCenter alignItemCenter' >
+        <div className='loginMainDiv'>
+           <div className='logInLogo'><img src={logo} /></div>
+           <div className='loginUserName '>
+            <input className='inputField' placeholder='username or email'/>
+          </div>
+           <div className='loginPassword '>
+            <input placeholder='password' className='inputField'/>
+          </div>
+           <button className='loginSubmit submitButton' >Log in</button>
+           <div className='loginSubmit submitButton'>Log in with facebook</div>
+           <div className='textAlignCenter forgotPassword'>Forgot password?</div>
         </div>
 
 
--- src/page/register/Register.js
+++ src/page/register/Register.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import "./register.css"
+import logo from "../../assests/download.png"
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className='registerBody  displayFlex justifyContentCenter alignItemCenter' >
+    <div className='registerMainDiv'>
+       <div className='registerLogo'><img src={logo} /></div>
+       <div className='registerUserName marginBotton '>
+        <input className='inputField' placeholder='Name'/>
+      </div>
+       <div className='registerUserName marginBotton '>
+        <input className='inputField' placeholder='Email'/>
+      </div>
+      <div className='registerPassword '>
+      <input placeholder='Password' className='inputField'/>
+      </div>
+      <div className='registerUserName marginBotton '>
+       <input className='inputField' placeholder='Confirm password'/>
+     </div>
+       <button className='registerSubmit submitButton' >Sign up</button>
+       <div className='loginSubmit submitButton'>Log in with facebook</div>
+       <div className='textAlignCenter forgotPassword'>Forgot password?</div>
+    </div>
+
+
+</div>
   )
 }
 
